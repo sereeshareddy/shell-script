ccdfb11c4387de2c9020152d64bb93c016eaa1f7
sereesha reddy <sereeshareddy971@gmail.com>
2024-03-07 12:39:27 +0530

    date script added

---
+++ 02-variables.sh
@@ -0,0 +1,7 @@
+
+#!/bin/bash
+
+#shell script  run the command inside $() and stroes the output into varaiable called DATE
+DATE=$(date)
+
+echo "Hello, this script is executed at timestamp: $DATE"
