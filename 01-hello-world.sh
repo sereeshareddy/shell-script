f98da3c77cf4a6f94116fba00b1186549e637b2f
sereesha reddy <sereeshareddy971@gmail.com>
2024-03-07 12:19:19 +0530

    hello script added

--- 01-hello-world.sh
+++ 01-hello-world.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-echo "Hello, I am learning Linux"
-echo "Ramesh: Hi,Suresh good morning"
-echo "suresh: Good morning Ramesh"
-echo "Ramesh: how are you doing"
+PERSON1=Ramesh
+PERSON2=SURESH
+echo "$PERSON1, I am learning Linux"
+echo "$PERSON2: Hi,$PERSON1 good morning"
+echo "$PERSON1: Good morning $PERSON2 "
+echo "$PERSON2: how are you doing $PERSON1"
