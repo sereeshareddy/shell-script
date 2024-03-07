d2af666b22fb8e93f0c7a70c7176c81ebfdb4eb5
sereesha reddy <sereeshareddy971@gmail.com>
2024-03-07 14:28:08 +0530

    variable script added

---
+++ 03-variables.sh
@@ -0,0 +1,8 @@
+
+#!/bin/bash
+PERSON1=$1
+PERSON2=$2
+echo "$PERSON1, I am learning Linux"
+echo "$PERSON2: Hi,$PERSON1 good morning"
+echo "$PERSON1: Good morning $PERSON2 "
+echo "$PERSON2: how are you doing $PERSON1"
