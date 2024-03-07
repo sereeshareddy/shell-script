1147f530ca73807409e7a1ee57a61115ddb64454
sereesha reddy <sereeshareddy971@gmail.com>
2024-03-07 19:06:12 +0530

    variable script added

--- 05-variables.sh
+++ 05-variables.sh
@@ -3,4 +3,11 @@
 echo "please enter your name"
 
 read USERNAME #THIS IS THE VARAIBLE WHERE THE VALUE OF USER ENTERED
+
 echo "Username enterted is: $USERNAME"
+
+echo "please enter your password"
+
+read PASSWORD
+
+echo "Password enterted is: $PASSWORD"
